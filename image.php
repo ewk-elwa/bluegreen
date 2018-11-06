f2a649167a6b9abb58d93c68e273f142e12ef0d3
ewk-elwa <edward.king@elwatech.com>
2018-11-06 14:03:29 -0800

    Fix it up

--- image.php
+++ image.php
@@ -5,16 +5,21 @@ $white = imagecolorallocate($im, 255, 255, 255);
 
 $deployment = getenv("COLOR");
 
+
 if ( $deployment == 'blue') {
+  $opposite = imagecolorallocate($im, 32, 255, 32);
   $color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
   $color = imagecolorallocate($im, 0, 255, 0);
+  $opposite = imagecolorallocate($im, 32, 32, 255);
 } else {
   $color = imagecolorallocate($im, 0, 0, 255);
+  $opposite = imagecolorallocate($im, 128, 128, 255);
 }
 
 // Draw a filled rectangle
 imagefilledrectangle($im, 0, 0, 199, 199, $color);
+imagefilledrectangle($im, 11, 22, 299, 399, $opposite);
 
 // Save the image
 header('Content-Type: image/png');
